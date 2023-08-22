2ffaed1ea12ed8e64b730b836445b67ba89673ea
DeshnaJain01 <128178878+DeshnaJain01@users.noreply.github.com>
2023-08-22 22:19:09 +0530

    Add files via upload

---
+++ multiSurveyForm/frontPage.js
@@ -0,0 +1,10 @@
+
+
+  // Get random index from array
+  function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+
+  // Set random images for left and right sides
+  document.getElementById('leftImg').src = communityImages[getRandomIndex(https://source.unsplash.com/random/1920x1080/?wallpaper,education)];
+  document.getElementById('rightImg').src = leadershipImages[getRandomIndex(https://source.unsplash.com/random/1920x1080/?wallpaper,leadership)];
